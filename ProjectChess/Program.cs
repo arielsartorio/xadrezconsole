53cb04f3f3de21343a6a66d6439cbaf036eb8930
Ariel Sartorio <ariel.sartorio@live.com>
2023-03-06 09:15:59 -0300

    Fazendo os primeiros testes de movimentação

--- ProjectChess/Program.cs
+++ ProjectChess/Program.cs
@@ -11,23 +11,23 @@ namespace ProjectChess
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.Tab);
+                    Console.WriteLine();
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 4));
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                Tela.imprimirTabuleiro(tab);
+                    partida.executaMovimento(origem, destino);
+                }
 
-                Console.WriteLine();
-                PosicaoXadrez pos = new PosicaoXadrez('e', 5);
-                Console.WriteLine(pos);
-                Console.WriteLine();
-                Console.WriteLine(pos.toPosicao());
+                Console.ReadLine();
             }
             catch (TabuleiroException e)
             {
