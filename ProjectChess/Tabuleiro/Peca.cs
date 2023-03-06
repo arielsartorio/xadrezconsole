2713143fbdaa5e9423470462cca979a37f4411f6
Ariel Sartorio <ariel.sartorio@live.com>
2023-03-06 19:53:13 -0300

    Correção: mudando o nome do método para movimentoPossivel

--- ProjectChess/Tabuleiro/Peca.cs
+++ ProjectChess/Tabuleiro/Peca.cs
@@ -28,7 +28,7 @@
             return false;
         }
 
-        public bool podeMoverPara(Posicao pos)
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.Linha, pos.Coluna];
         }
